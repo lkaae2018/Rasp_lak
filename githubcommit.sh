e6ff2a6ed4bb19c652a20d591c393b9a25215377
lkaae2018 <40848476+lkaae2018@users.noreply.github.com>
2021-02-27 10:16:20 +0100

    Update githubcommit.sh
    Github

--- githubcommit.sh
+++ githubcommit.sh
@@ -2,8 +2,8 @@
 
 #cd linuxopg
 sudo git add *
-sudo git config --global user.email "lak@djhhadsten.dk"
-sudo git config --global user.name "lkaae2018"
+sudo git config --global user.email "*******"
+sudo git config --global user.name "**********"
 echo Navn til commiten?
 read commit
 sudo git commit -m $commit
